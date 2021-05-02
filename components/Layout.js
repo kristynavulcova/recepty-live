65fcb16e0acc0b9f9496e3fef9a37f27f05c7da8
kristynavulcova <kristynavulcova@gmail.com>
2021-05-02 14:05:07 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ components/Layout.js
@@ -0,0 +1,26 @@
+import Link from 'next/link'
+
+export default function Layout({ children }) {
+  return (
+    <div className="layout">
+      <header>
+        <Link href="/">
+          <a>
+            <h1>
+              <span>Recepty</span>
+            </h1>
+            <h2>Plné radosti</h2>
+          </a>
+        </Link>
+      </header>
+
+      <div className="page-content">
+        { children }
+      </div>
+
+      <footer>
+        <p>Copyright 2021 Recepty :)</p>
+      </footer>
+    </div>
+  )
+}
---
+++ pages/recipes/[slug].js
@@ -0,0 +1,109 @@
+import { createClient } from "contentful"
+import Image from "next/image"
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+
+const client = createClient({
+  space:process.env.CONTENTFUL_SPACE_ID,
+  accessToken:process.env.CONTENTFUL_ACCESS_KEY,
+})
+
+export const getStaticPaths = async () => {
+
+  const response = await client.getEntries({
+    content_type: "recipe"
+  })
+
+
+  const paths = response.items.map(item => {
+  return{
+    params:{slug:item.fields.slug}
+  }
+})
+
+  return {
+    paths,
+    fallback:false
+  }
+  }
+
+  export async function getStaticProps ({params}){
+
+    const {items} = await client.getEntries(
+      {
+        content_type: "recipe",
+        "fields.slug":params.slug
+      }
+    )
+ 
+    return{
+      props: { recipe: items[0] },
+      revalidate:1
+    }
+  }
+
+
+export default function RecipeDetails({recipe}) {
+  const {featuredImage, title,cookingTime,ingredients,method} = recipe.fields
+  return (
+    <div>
+      <div className="banner">
+<Image
+src={"https:" + featuredImage.fields.file.url}
+width={900}
+height={400}
+objectFit="cover"
+className="banner-img"
+        />
+        <h2>{title}</h2>
+      </div>
+      <div className="info">
+        <p>Doba přípravy: {cookingTime} minut</p>
+        <h3>Ingredience:</h3>
+        {ingredients.map(ing => (
+          <span key={ing}>{ing}</span>
+        ))}
+      </div>
+      <div className="method">
+        <h3> Postup přípravy:</h3>
+        <div>
+          {documentToReactComponents(method)}
+        </div>
+      </div>
+   <style jsx>
+     {
+`
+.banner{
+  text-align: center;
+}
+.banner-img{
+  margin: 0 auto;
+}
+h2,h3 {
+  text-transform: uppercase;
+}
+.banner h2 {
+  margin: 0;
+  background: #fff;
+  display: inline-block;
+  padding: 20px;
+  position: relative;
+  top: -60px;
+  left: -10px;
+  transform: rotateZ(-1deg);
+  box-shadow: 1px 3px 5px rgba(0,0,0,0.1);
+}
+.info p {
+  margin: 0;
+}
+.info span::after {
+  content: ", ";
+}
+.info span:last-child::after {
+  content: ".";
+}
+`
+     }
+     </style>
+    </div>
+  )
+}
